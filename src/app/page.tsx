9eadb9aa9fd8f8362bdad0c21218cb23e2ccfbf6
Subhradeep1708 <subhradeep1708@gmail.com>
2025-07-13 02:04:23 +0530

    refactor: remove unused Image import from page and 3d-card components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
-import Image from "next/image";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
